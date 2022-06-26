4f95b91f233cfe1777054616000907b5cf13e35e
King AJ <talk2king.aj@gmail.com>
2022-06-26 16:40:39 +0100

    custom video player completed

---
+++ Day 3 custom video player/script.js
@@ -0,0 +1,48 @@
+const video = document.getElementById('video');
+const play = document.getElementById('play');
+const stop = document.getElementById('stop');
+const progress = document.getElementById('progress');
+const timestamp = document.getElementById('timestamp');
+
+const toggleVideoStatus = () => {
+    if(video.paused){
+        video.play();
+    }else {
+        video.pause();
+    }
+}
+
+const changePlayIcon = () =>{
+if(video.paused){
+    play.innerHTML= '<i class="fa fa-play fa-2x"></i>';
+}else {
+    play.innerHTML= '<i class="fa fa-pause fa-2x"></i>';
+}
+}
+
+const stopVideo =() =>{
+    video.currentTime = 0;
+    video.pause();
+}
+
+const updateProgress = () => {
+progress.value = (video.currentTime / video.duration) * 100;
+let mins = Math.floor(video.currentTime/60);
+if(mins< 10){mins = '0'+String(mins);}
+let secs = Math.floor(video.currentTime % 60);
+if(secs< 10){secs = '0'+String(secs);}
+timestamp.innerText = `${mins}:${secs}`
+
+}
+
+const setProgress = ()=> {
+    video.currentTime = (+progress.value * video.duration) / 100;
+}
+
+video.addEventListener('click', toggleVideoStatus);
+video.addEventListener('click', changePlayIcon);
+video.addEventListener('timeupdate', updateProgress)
+play.addEventListener('click', toggleVideoStatus);
+play.addEventListener('click', changePlayIcon);
+stop.addEventListener('click', stopVideo);
+progress.addEventListener('change', setProgress);
